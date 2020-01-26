0f783189083eee63ccc53daf49e629947beb3352
lucafriggi <lucafriggieri@gmail.com>
2020-01-26 16:57:19 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   imports: [
@@ -24,8 +23,7 @@ import { ProductsComponent } from './products/products.component';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent,
-    ProductsComponent
+    ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ]
 })
